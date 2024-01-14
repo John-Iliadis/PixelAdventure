0c447316f8f0f92e4ad7a0ff26b82ef11eb52a53
Ioannis Iliadis <giannisiliadis2k15@gmail.com>
2024-01-14 17:42:49 +1100

    Created tile map class

---
+++ src/world/tile_map.cpp
@@ -0,0 +1,65 @@
+//
+// Created by Gianni on 13/01/2024.
+//
+
+#include "tile_map.hpp"
+
+
+using json = nlohmann::json;
+
+constexpr int tile_scale = 2;
+
+TileMap::TileMap(GameContext& context, const std::string &file_name)
+{
+    std::ifstream file(file_name);
+
+    if (!file.is_open())
+    {
+        throw std::runtime_error("TileMap::TileMap - Failed to open file: " + file_name);
+    }
+
+    json map_data = json::parse(file);
+
+    window_size = context.window->getSize();
+
+    m_map_sprite.setTexture(context.texture_manager->get("test_map"));
+    m_map_sprite.setScale(tile_scale, tile_scale);
+
+    const uint32_t tile_width = map_data["tilewidth"].get<uint32_t>();
+    const uint32_t tile_height = map_data["tileheight"].get<uint32_t>();
+
+    const uint32_t total_tiles_x = map_data["width"].get<uint32_t>();
+    const uint32_t total_tiles_y = map_data["height"].get<uint32_t>();
+
+    std::vector<int> tile_codes = map_data["layers"][0]["data"].get<std::vector<int>>();
+
+    for (int i = 0; i < total_tiles_y; ++i)
+    {
+        for (int j = 0; j < total_tiles_x; ++j)
+        {
+            if (tile_codes.at(i * total_tiles_x + j) != 0)
+            {
+                sf::FloatRect collider_rect {
+                        static_cast<float>(j * tile_width * tile_scale),
+                        static_cast<float>(i * tile_height * tile_scale),
+                        static_cast<float>(tile_width * tile_scale),
+                        static_cast<float>(tile_height * tile_scale)
+                };
+
+                m_colliders.push_back(std::move(collider_rect));
+            }
+        }
+    }
+
+    file.close();
+}
+
+const std::vector<sf::FloatRect>& TileMap::get_colliders() const
+{
+    return m_colliders;
+}
+
+void TileMap::draw(sf::RenderTarget &target, sf::RenderStates states) const
+{
+    target.draw(m_map_sprite);
+}
---
+++ src/world/tile_map.hpp
@@ -0,0 +1,37 @@
+//
+// Created by Gianni on 13/01/2024.
+//
+
+#ifndef PLATFORMER_TILE_MAP_HPP
+#define PLATFORMER_TILE_MAP_HPP
+
+#include <vector>
+#include <fstream>
+#include <cstdint>
+#include <memory>
+#include <SFML/Graphics/Rect.hpp>
+#include <SFML/Graphics/Sprite.hpp>
+#include <SFML/Graphics/RenderWindow.hpp>
+#include "../json.hpp"
+#include "../states/game_context.hpp"
+#include "../asset_managers/texture_manager.hpp"
+
+
+class TileMap : public sf::Drawable
+{
+public:
+    TileMap(GameContext& context, const std::string& file_name);
+
+    const std::vector<sf::FloatRect>& get_colliders() const;
+
+private:
+    void draw(sf::RenderTarget &target, sf::RenderStates states) const override;
+
+private:
+    std::vector<sf::FloatRect> m_colliders;
+    sf::Sprite m_map_sprite;
+    sf::Vector2u window_size;
+};
+
+
+#endif //PLATFORMER_TILE_MAP_HPP
