f81b122afd9441e7f4239f9076a72f814266e0a6
Ioannis Iliadis <giannisiliadis2k15@gmail.com>
2024-01-19 16:19:41 +1100

    Added new utility functions

--- src/utilities/utils.cpp
+++ src/utilities/utils.cpp
@@ -4,6 +4,7 @@
 
 #include "utils.hpp"
 
+
 uint32_t get_max_refresh_rate()
 {
     DEVMODE dev_mode;
@@ -14,4 +15,32 @@ uint32_t get_max_refresh_rate()
         return dev_mode.dmDisplayFrequency;
     else
         throw std::runtime_error("get_max_refresh_rate() - Failed to retrieve monitor refresh rate\n");
+}
+
+void set_origin_center(sf::Sprite& sprite)
+{
+    auto rect = sprite.getLocalBounds();
+
+    sprite.setOrigin(rect.width / 2.f, rect.height / 2.f);
+}
+
+void set_origin_top_left(sf::Sprite& sprite)
+{
+    sprite.setOrigin(0, 0);
+}
+
+void flip_sprite_x(sf::Sprite& sprite, bool facing_right)
+{
+    auto rect = sprite.getTextureRect();
+
+    if (facing_right)
+    {
+        rect = {0, 0, -rect.width, rect.height};
+    }
+    else
+    {
+        rect = {rect.width, 0, -rect.width, rect.height};
+    }
+
+    sprite.setTextureRect(rect);
 }
--- src/utilities/utils.hpp
+++ src/utilities/utils.hpp
@@ -8,7 +8,12 @@
 #include <windows.h>
 #include <cstdint>
 #include <stdexcept>
+#include <SFML/Graphics/Sprite.hpp>
 
 uint32_t get_max_refresh_rate();
+
+void set_origin_center(sf::Sprite& sprite);
+void set_origin_top_left(sf::Sprite& sprite);
+void flip_sprite_x(sf::Sprite& sprite, bool facing_right);
 
 #endif //PLATFORMER_UTILS_HPP
