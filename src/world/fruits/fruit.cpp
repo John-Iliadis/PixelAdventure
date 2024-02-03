a469f15e2710ee4f4c475a742c065a11a5ccd3d5
Ioannis Iliadis <giannisiliadis2k15@gmail.com>
2024-02-04 10:34:25 +1100

    Created fruit class

---
+++ src/world/fruits/fruit.cpp
@@ -0,0 +1,34 @@
+//
+// Created by Gianni on 4/02/2024.
+//
+
+#include "fruit.hpp"
+
+
+static const sf::Vector2f collider_size = {18, 20};
+
+Fruit::Fruit(const sf::Texture &texture, const sf::Vector2f &position)
+{
+    m_animation = Animation(32, 32, 17, sf::milliseconds(50), true);
+
+    m_fruit.set_texture(texture);
+    m_fruit.set_position(position);
+    m_fruit.set_collider_size(collider_size);
+    m_fruit.set_origin(Origin::CENTER);
+}
+
+void Fruit::update(double dt)
+{
+    m_animation.update(dt);
+    m_fruit.set_texture_rect(m_animation.get_current_frame_rect());
+}
+
+sf::Rect<float> Fruit::get_collider() const
+{
+    return m_fruit.get_collider();
+}
+
+void Fruit::draw(sf::RenderTarget& target, sf::RenderStates states) const
+{
+    target.draw(m_fruit);
+}
---
+++ src/world/fruits/fruit.hpp
@@ -0,0 +1,30 @@
+//
+// Created by Gianni on 4/02/2024.
+//
+
+#ifndef PLATFORMER_FRUIT_HPP
+#define PLATFORMER_FRUIT_HPP
+
+#include "../../animation/animation.hpp"
+#include "../sprite_collider.hpp"
+
+
+class Fruit : public sf::Drawable
+{
+public:
+    Fruit(const sf::Texture& texture, const sf::Vector2f& position);
+
+    void update(double dt);
+
+    sf::Rect<float> get_collider() const;
+
+private:
+    void draw(sf::RenderTarget &target, sf::RenderStates states) const override;
+
+private:
+    SpriteCollider m_fruit;
+    Animation m_animation;
+};
+
+
+#endif //PLATFORMER_FRUIT_HPP
