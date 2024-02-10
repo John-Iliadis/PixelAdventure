7813a7e9191f019d02fb65e6bcc427c2c7ab56b9
Ioannis Iliadis <giannisiliadis2k15@gmail.com>
2024-02-10 12:16:25 +1100

    includes all gui elements

---
+++ src/gui/gui_includes.hpp
@@ -0,0 +1,16 @@
+//
+// Created by Gianni on 10/02/2024.
+//
+
+#ifndef PLATFORMER_GUI_INCLUDES_HPP
+#define PLATFORMER_GUI_INCLUDES_HPP
+
+#include "button.hpp"
+#include "text_button.hpp"
+#include "gui_container.hpp"
+#include "slider.hpp"
+#include "sprite_element.hpp"
+#include "text_element.hpp"
+#include "gui_builder.hpp"
+
+#endif //PLATFORMER_GUI_INCLUDES_HPP
