2160330895a9c18ee20b3b02c2f59cd8c96071d2
Ioannis Iliadis <giannisiliadis2k15@gmail.com>
2024-07-18 12:41:32 +1000

    created gui button

--- src/gui/gui_button.cpp
+++ src/gui/gui_button.cpp
@@ -3,3 +3,107 @@
 //
 
 #include "gui_button.hpp"
+
+
+GUI_Button::GUI_Button()
+{
+    m_text.set_parent(&m_sprite);
+}
+
+GUI_Button::GUI_Button(const sf::Texture &texture, const sf::Font &font, uint32_t char_size, sf::String string)
+    : m_sprite(texture)
+    , m_text(font, char_size, string)
+{
+    m_text.set_parent(&m_sprite);
+}
+
+void GUI_Button::set_texture(const sf::Texture &texture)
+{
+    m_sprite.set_texture(texture);
+}
+
+void GUI_Button::set_font(const sf::Font &font)
+{
+    m_text.set_font(font);
+}
+
+void GUI_Button::set_char_size(uint32_t size)
+{
+    m_text.set_char_size(size);
+}
+
+void GUI_Button::set_string(const sf::String &string)
+{
+    m_text.set_string(string);
+}
+
+void GUI_Button::set_text_color(const sf::Color &color)
+{
+    m_text.set_color(color);
+}
+
+void GUI_Button::set_pos(float x, float y)
+{
+    m_sprite.set_pos(x, y);
+}
+
+void GUI_Button::set_scale(float scale)
+{
+    m_sprite.set_scale(scale);
+}
+
+void GUI_Button::set_origin(float x, float y)
+{
+    m_sprite.set_origin(x, y);
+}
+
+void GUI_Button::handle_event(const sf::Event &event)
+{
+}
+
+void GUI_Button::draw(sf::RenderWindow &window)
+{
+    m_sprite.draw(window);
+    m_text.draw(window);
+}
+
+bool GUI_Button::selectable()
+{
+    return true;
+}
+
+void GUI_Button::activate()
+{
+    m_callback();
+}
+
+sf::FloatRect GUI_Button::local_bb() const
+{
+    return m_sprite.local_bb();
+}
+
+sf::FloatRect GUI_Button::global_bb() const
+{
+    return transform().transformRect(local_bb());
+}
+
+sf::Transform GUI_Button::transform() const
+{
+    return m_sprite.transform();
+}
+
+void GUI_Button::select()
+{
+    if (!selected())
+    {
+        GUI_Element::select();
+    }
+}
+
+void GUI_Button::deselect()
+{
+    if (selected())
+    {
+        GUI_Element::deselect();
+    }
+}
--- src/gui/gui_button.hpp
+++ src/gui/gui_button.hpp
@@ -5,9 +5,45 @@
 #ifndef PIXEL_ADVENTURE_GUI_BUTTON_HPP
 #define PIXEL_ADVENTURE_GUI_BUTTON_HPP
 
-class GUI_Button
-{
+#include <functional>
+#include <SFML/Graphics/Texture.hpp>
+#include "gui_element.hpp"
+#include "gui_sprite.hpp"
+#include "gui_text.hpp"
 
+
+class GUI_Button : public GUI_Element
+{
+public:
+    GUI_Button();
+    GUI_Button(const sf::Texture& texture, const sf::Font& font, uint32_t char_size, sf::String string = "");
+
+    void set_texture(const sf::Texture& texture);
+    void set_font(const sf::Font& font);
+    void set_char_size(uint32_t size);
+    void set_string(const sf::String& string);
+    void set_text_color(const sf::Color& color);
+    void set_pos(float x, float y) override;
+    void set_scale(float scale) override;
+    void set_origin(float x, float y) override;
+
+    void handle_event(const sf::Event &event) override;
+    void draw(sf::RenderWindow &window) override;
+
+    void select() override;
+    void deselect() override;
+
+    bool selectable() override;
+    void activate() override;
+
+    sf::FloatRect local_bb() const override;
+    sf::FloatRect global_bb() const override;
+    sf::Transform transform() const override;
+
+private:
+    GUI_Sprite m_sprite;
+    GUI_Text m_text;
+    std::function<void()> m_callback;
 };
 
 
